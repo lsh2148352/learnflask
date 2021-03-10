58dfad5f018dc6e9e8dfd8b06c9ab86f7f3f7276
L <491626402@qq.com>
2021-03-10 20:01:59 +0800

    接收不同类型的请求

--- index.py
+++ index.py
@@ -1,12 +1,19 @@
-from flask import Flask,Blueprint
+from flask import Flask,Blueprint,request
 
 
 index_page = Blueprint("index_page",__name__)
 
-@index_page.route("/1")
+@index_page.route("/1",methods = ["GET"])
 def test1():
-    return "hello 112"
+    re = request.values
+    return re["a"] if "a" in re else "hello 112"
 
-@index_page.route("/2")
+@index_page.route("/2",methods = ["GET","POST"])
 def test2():
-    return"hello 2"
+    re = request.values#get和post都可以
+    return re["a"] if "a" in re else "hello 112"
+
+@index_page.route("/upload",methods = ["POST"])
+def test3():
+    re = request.files['file']  if "file" in request.files else None#这里要跟上传参数的键名一致
+    return "%s"%(re)
--- test.py
+++ test.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.register_blueprint(index_page,url_prefix="/l")
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0',port=5000,debug=True)
+    app.run(host='127.0.0.1',port=5000,debug=True)
     # c = {"l":"123"}
     # print(c)
     # c["u"] = {"name":"lanzi","qq":"456"}
